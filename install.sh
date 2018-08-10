539b7fa690455ddaa169f0e14c2d152b99de9b76
Casxt <774714620@qq.com>
2018-08-10 12:30:55 +0000

    improve

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ unzip master.zip
 rm -f master.zip
 go get github.com/aliyun/alibaba-cloud-sdk-go/sdk
 go build -o DDNS-master/DDNS DDNS-master/AliYun-DDNS.go 
-sudo cp DDNS-master/DDNS /etc/bin/DDNS
+sudo cp DDNS-master/DDNS /usr/bin/DDNS
 sudo mkdir /etc/DDNS
 sudo cp DDNS-master/config.template.json /etc/DDNS/config.json
 sudo cp DDNS-master/ddns.service /etc/systemd/system/ddns.service
-echo "1. edit /etc/DDNS/config.json 
+echo "1. nano /etc/DDNS/config.json 
       2. run 'sudo systemctl start ddns'"
