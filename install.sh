2743c09799cf503fd51c4e3ddae7eced67e1ea54
casxt <774714620@qq.com>
2018-08-10 20:16:12 +0800

    bugfix

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #bash
 wget https://github.com/Casxt/DDNS/archive/master.zip
-unzip master.zip -d "ddns-src"
+unzip master.zip
 rm -f master.zip
 go get github.com/aliyun/alibaba-cloud-sdk-go/sdk
-go build ddns-src/AliYun-DDNS.go -o ddns-src/DDNS
-sudo cp ddns-src/DDNS /etc/bin/DDNS
+go build DDNS-master/AliYun-DDNS.go -o DDNS-master/DDNS
+sudo cp DDNS-master/DDNS /etc/bin/DDNS
 sudo mkdir /etc/DDNS
-sudo cp ddns-src/config.template.json /etc/DDNS/config.json
-sudo cp ddns-src/ddns.service /etc/systemd/system/ddns.service
+sudo cp DDNS-master/config.template.json /etc/DDNS/config.json
+sudo cp DDNS-master/ddns.service /etc/systemd/system/ddns.service
 echo "1. edit /etc/DDNS/config.json 
       2. run 'sudo systemctl start ddns'"
